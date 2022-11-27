4c11f3edcb06b216d692efcd2a3d5c35190c92ad
smaicas <samuel.maicas.development@gmail.com>
2022-11-27 22:40:57 +0100

    Format && Disclaimer

--- Samples/DOTNET/MVVM/Dnj.Colab.Samples.MvvmSample.Abstractions/DnjTodoViewModel.cs
+++ Samples/DOTNET/MVVM/Dnj.Colab.Samples.MvvmSample.Abstractions/DnjTodoViewModel.cs
@@ -1,4 +1,12 @@
-﻿using System.ComponentModel;
+﻿/* This file is copyright © 2022 Dnj.Colab repository authors.
+
+Dnj.Colab content is distributed as free software: you can redistribute it and/or modify it under the terms of the General Public License version 3 as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+Dnj.Colab content is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the General Public License version 3 for more details.
+
+You should have received a copy of the General Public License version 3 along with this repository. If not, see <https://github.com/smaicas-org/Dnj.Colab/blob/dev/LICENSE>. */
+
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using Dnj.Colab.Samples.MvvmSample.RCL.Model;
 using Dnj.Colab.Samples.MvvmSample.RCL.ViewModels;
@@ -7,18 +15,12 @@ namespace Dnj.Colab.Samples.MvvmSample.ViewModels.Abstractions;
 
 public abstract class DnjTodoViewModel : IDnjTodoViewModel
 {
+    private List<TodoItem> _toDoItemList = new();
+    private bool isBusy;
+
+    private TodoItem toDoItem = new();
     public event PropertyChangedEventHandler? PropertyChanged;
 
-    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-
-    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
-    {
-        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
-        field = value;
-        OnPropertyChanged(propertyName);
-        return true;
-    }
-    private bool isBusy = false;
     public bool IsBusy
     {
         get => isBusy;
@@ -29,7 +31,6 @@ public abstract class DnjTodoViewModel : IDnjTodoViewModel
         }
     }
 
-    private List<TodoItem> _toDoItemList = new();
     public virtual List<TodoItem> TodoItemList
     {
         get => _toDoItemList;
@@ -38,10 +39,8 @@ public abstract class DnjTodoViewModel : IDnjTodoViewModel
             _toDoItemList = value;
             OnPropertyChanged();
         }
-
     }
 
-    private TodoItem toDoItem = new();
     public TodoItem TodoItem
     {
         get => toDoItem;
@@ -51,23 +50,33 @@ public abstract class DnjTodoViewModel : IDnjTodoViewModel
             OnPropertyChanged();
         }
     }
+
     public int TodoItems => TodoItemList.Count(i => i.Done.Equals(false));
 
     public virtual async Task SaveTodoItem(TodoItem todoitem)
     {
         IsBusy = true;
-        if (todoitem.Id.Equals(default(Guid)))
-        {
+        if (todoitem.Id.Equals(default))
             todoitem.Id = Guid.NewGuid();
-        }
         else
-        {
             _toDoItemList.Remove(todoitem);
-        }
 
         _toDoItemList.Add(todoitem);
 
         OnPropertyChanged(nameof(TodoItemList));
         IsBusy = false;
+    }
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
     }
 }
